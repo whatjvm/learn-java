e7e6e3e377bc680e701186a43ec279a1392ce7d8
zhijie wu <1005904853@qq.com>
2020-03-02 16:40:09 +0800

    update Bookervice.java

--- section-1-book-management/src/com/diamondfsd/learnjava/service/BookService.java
+++ section-1-book-management/src/com/diamondfsd/learnjava/service/BookService.java
@@ -2,19 +2,23 @@ package com.diamondfsd.learnjava.service;
 
 import com.diamondfsd.learnjava.dao.BookDAO;
 import com.diamondfsd.learnjava.dao.MemoryBookDAOImpl;
+import com.diamondfsd.learnjava.dao.MemoryInFileBookDAOImpl;
 import com.diamondfsd.learnjava.entity.Book;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
 public class BookService {
-    private BookDAO bookDAO = new MemoryBookDAOImpl();
 
     private static final byte BOOK_TIME_SORT_CONSOL = 1;
     private static final byte BOOK_PRICE_SORT_CONSOL = 2;
     private static final byte BOOK_NAME_SORT_CONSOL = 3;
+
+//    private BookDAO bookDAO = new MemoryBookDAOImpl();
+    private BookDAO bookDAO = new MemoryInFileBookDAOImpl();
+
 
     // 创建图书的时候调用
     public void saveBook(Book book) {
@@ -30,50 +34,27 @@ public class BookService {
         return bookDAO.getById(id);
     }
 
-    public List<Book> listBookSortByName() {
-        return this.bubbleSort(allBook(),BOOK_NAME_SORT_CONSOL);
+    public void DeleteBook(Integer id){
+            bookDAO.BookDelete(id);
     }
 
-    public List<Book> listBookSortByPrice(){
-        return this.bubbleSort(allBook(),BOOK_PRICE_SORT_CONSOL);
+    public void EditBook(Integer id , Book book){
+            bookDAO.BookEdit(id , book);
     }
 
     public List<Book> listBookSortByTime(){
-        return this.bubbleSort(allBook(),BOOK_TIME_SORT_CONSOL);
+        return this.bubbleSort(bookDAO.allBook() , BOOK_TIME_SORT_CONSOL);
     }
 
-    public void inIDofDeledBook(Integer id){
-            bookDAO.inIDofBookDeled(id);
+    public List<Book> listBookSortByName(){
+        return this.bubbleSort(bookDAO.allBook() , BOOK_NAME_SORT_CONSOL);
     }
 
-    public void inIDofEditBook(Integer id , Book book){
-            bookDAO.inIDofBookEdit(id,book);
+    public List<Book> listBookSortByPrice(){
+        return this.bubbleSort(bookDAO.allBook() , BOOK_PRICE_SORT_CONSOL);
     }
 
-    /**
-     * 保存数据到文本
-     */
-    public void storeDataOutFile(){
-        if(bookDAO.allBook().size() != 0){
-            bookDAO.outputFile();
-        }
-    }
-
-    /**
-     * 载入数据
-     */
-    public void inputFileData(){
-            bookDAO.inputFile();
-    }
-
-    public Boolean bookIDisTrue(Integer id){
-        if(id.compareTo(bookDAO.getById(id).getId()) == 0){
-            return true;
-        }
-        return false;
-    }
-
-    private List<Book> bubbleSort(List<Book> books,byte consol){
+    private List<Book> bubbleSort(List<Book> books, byte consol){
         for(int i = 0;i < books.size();i++){
             for(int j = 0;j < books.size();j++){
                 if(decideConsol(books.get(i) , books.get(j) , consol)){
@@ -88,10 +69,10 @@ public class BookService {
 
     /**
      * @param bookOne 冒泡排序的第一个book
-     * @param bookNext冒泡排序的第二个book
+     * @param bookNext 冒泡排序的第二个book
      * @param consolByte 操作该进入哪个case进行转换的字符串。
      * @return 返回 bookOne 与 bookNext 的对比结果.
-     */
+     **/
     private Boolean decideConsol(Book bookOne , Book bookNext , byte consolByte){
         double bookOneValue = 0;
         double bookNextValue = 0;
@@ -118,6 +99,7 @@ public class BookService {
         return false;
 
     }
+
 
 }
 
